a4497e1244616cae1150db5049dd70848edb01da
ReeceM <reecestevenmay@gmail.com>
2021-02-22 01:08:33 +0200

    changes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,11 @@
 module.exports = {
-  stories: ["../slices/**/*.stories.[tj]s"],
+  stories: [
+    "../slices/**/*.stories.[tj]s",
+    '../components/**/*.stories.js'
+  ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    '@storybook/addon-controls'
   ]
 }
