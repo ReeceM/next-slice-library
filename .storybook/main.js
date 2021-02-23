94b6e2f00525fa8de3a1f350feb40d3c1b57399a
ReeceM <reecestevenmay@gmail.com>
2021-02-23 09:34:10 +0200

    try fix resolves

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,13 @@ module.exports = {
     // "@storybook/addon-links",
     "@storybook/addon-essentials",
     // '@storybook/addon-controls'
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve = {
+      ...config.resolve,
+      extensions: ['.wasm', '.js', '.mjs', '.json'],
+    }
+    // Return the altered config
+    return config;
+  }
 }
