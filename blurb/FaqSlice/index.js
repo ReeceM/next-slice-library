f991aecb0452c36f0504bc125ffa0ce9e5de6d60
ReeceM <reecestevenmay@gmail.com>
2021-02-23 22:24:54 +0200

    add schema.org for FAQ

--- blurb/FaqSlice/index.js
+++ blurb/FaqSlice/index.js
@@ -5,6 +5,7 @@ import { array, shape, string } from 'prop-types';
 import { Slice, Container, FAQ, Eyebrow } from '../../components'
 import { jsx, Box } from 'theme-ui';
 import { RichText } from 'prismic-reactjs';
+import Head from 'next/head'
 
 const FaqSlice = ({ slice }) => (
   <Slice>
@@ -26,10 +27,36 @@ const FaqSlice = ({ slice }) => (
       </Box>
 
       <Box as="div" sx={{ marginTop: '3rem' }}>
-          {slice?.items ? slice.items.map((item, index) => (
-            <FAQ faq={item} key={index} />
-          )) : null}
+        {slice?.items ? slice.items.map((item, index) => (
+          <FAQ faq={item} key={index} />
+        )) : null}
       </Box>
+
+      {
+        slice?.items
+          ? (
+            <Head>
+              <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                  __html: JSON.stringify({
+                    "@context": "https://schema.org",
+                    "@type": "FAQPage",
+                    "mainEntity": [slice.items.map((item) => {
+                      return {
+                        "@type": "Question",
+                        "name": item.question,
+                        "acceptedAnswer": {
+                          "@type": "Answer",
+                          "text": RichText.asText(item.answer)
+                        }
+                      }
+                    })]
+                  })
+                }} />
+            </Head>
+          )
+          : null}
     </Container>
   </Slice>
 )
@@ -41,7 +68,11 @@ FaqSlice.propTypes = {
       title: array.isRequired,
       eyebrow: string,
       description: array,
-    }).isRequired
+    }).isRequired,
+    items: [{
+      question: string,
+      answer: array
+    }]
   }).isRequired,
 };
 
